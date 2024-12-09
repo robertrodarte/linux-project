c97b8c326abd2876d87c5c06ce4ee5abca82892a
Robert Rodarte <rrodarte@localhost.localdomain>
2024-12-08 18:02:05 -0800

    Didn't notice I was in my logs directory on last commit

--- README.md
+++ README.md
@@ -1,17 +1,38 @@
 Project Title: Linux Diagnostics Script
 
-Purpose: I created this project because I have little to no experience with Linux and wanted to begin learning. I am using RHEL on 
-a virtual machine set up with Oracle Virtual Box. 
+Purpose: I created this project to start learning Linux, as I have little to no prior experience with it. I am using the RHEL operating system on a virtual machine set up with Oracle VirtualBox.  
 
-Features: ... coming soon
+Features: 
+1. Monitors the connectivity and status of network devices
+2. Monitors the services on a system
 
-Commands: ... coming soon
+Commands:
+echo: Outputs a text to a file or terminal
+	> $FILE_NAME : Redirects the output to the file specified. If the file exists, it overwrites
+	>> $FILE_NAME: Appends the text to the end of the file specified instead of overwriting it
 
-How To Run: ... coming soon
+ping: Sends a small packet to the target device and waits for reply to check if its reachable
+	-c #: Sends exactly # of packets and then stops
+	&> /dev/null: Redirects standard output and standard error messsages to /dev/null, which discards them
+
+df: Displays the disk space usage
+	-h: Displays the disk space usage in human readable format
+
+free: Shows memory usage
+	-m: Shows memory usage in megabytes
+
+systemctl: Check status of any systemd service running
+	is-active: Checks if a particular service is currently running (returns active or inactive status)
+	--quiet: Supresses the output to the terminal 
+
+How To Run:
+1. Use ./diagnostics.sh to run script
+2. View diagnostics.log
 
 What I learned:
-1. I learned how to use Oracle Virtual Box to create my own virtual machine and how to utilize RHEL (Red Hat Enterprise Linux)
-2. I learned the basics of the terminal using commands like cd, ls, mkdir... and many more
-3. I learned the basics of scripting using commands like echo, ping, curl.. and many more
+1. I learned how to use Oracle VirtualBox to create my own virtual machine and how to utilize the RHEL (Red Hat Enterprise Linux) OS
+2. I learned the basics of the terminal using commands like cd, ls, touch... and more
+3. I learned the basics of scripting using commands like echo, ping, curl... and more
 4. I learned how to utilize an SSH key and connect it to my Git account to push my changes 
-5. ... in progress
+5. I learned how to use VIM to edit files in the terminal
+6. I learned when a Linux OS would be used in an embedded system and how to utilize it.
--- scripts/diagnostics.sh
+++ scripts/diagnostics.sh
@@ -1,63 +1,55 @@
 # Shebang - Indicates which interpreter to use to run script (bash)
 #! /usr/bin/bash
 
-# Sets a variable to store the path of the log file where results will be written
 LOG_FILE=~/linux_project/logs/diagnostics.log
-# Sets a variable to store the path of the input file that contains the list of device IP addresses
 DEVICES_FILE=~/linux_project/configs/devices.txt # Sets DEVICES_FILE to file with list of devices
-# An array of services to check 
 SERVICES=("sshd" "httpd")
 
+# $(date): Command to display date and time
 echo "Running Diagnostics - $(date)"
-# Base script to automate 
-# -----------------------------------------
-# echo : Ouputs text to a file or terminal 
-# $(date) : Runs the date command to get the current date and time
-# > : Redirects the output to the file specified by $LOG_FILE. If the file exists, it overwrites.
 echo "Running Diagnostics - $(date)" > $LOG_FILE
 
-# Test Device Connectivity 
-# >> : Appends the text to the end of the $LOG_FILE instead of overwriting it
-echo "Checking device connectivity:" >> $LOG_FILE
-# Starts loop that runs as long as there are lines to read
+# Test Device Connectivity
+echo -e "\nChecking device connectivity:" >> $LOG_FILE
+
 # IFS= : Temporarily disables the default input field separators so lines with spaces aren't split incorrectly
-# read -r device : Reads each line from the input file into the variable "device"
 while IFS= read -r device; do
-	# ping : sends a small packet to the target device and waits for reply to check if its reachable
-	# -c 3 : Sends exactly 3 packets and then stops
-	# &> /dev/null : Redirects both standard output (normal messages) and standard error (error messages) 
-	# 		 to /dev/null, which discards them. 
+	# Ping the device
 	ping -c 3 $device &> /dev/null
-	# $? : Stores the exit code of the last command (ping in this case). A value of zero means success, while anything 
-	#      else indicates failure
-	# -eq 0 : Compares the exit code to 0 (-eq = equal)	
+
+	# Check if ping command returns a 0 (success) or fail(>0)	
 	if [ $? -eq 0 ]; then 
 		echo "$device is reachable" >> $LOG_FILE
 	else 
 		echo "ERROR: $device is unreachable" >> $LOG_FILE
 	fi 
-	
-	# df -h : Displays the disk space usage in a human-readable format(-h) 
-	echo "Disk Usage:" >> $LOG_FILE
-	df -h >> $LOG_FILE
-	# free -m : Shows memory usage in megabytes(-m)  
-	echo "MEMORY USAGE:" >> $LOG_FILE
-	free -m >> $LOG_FILE
-	
-	# Iterate through each service in the array 
-	for service in "${SERVICES[@]}"; do
-		# Checks if the service is runnning 
-		# --quiet : supresses the output to the terminal; exit code used instead
-		systemctl is-active --quiet $service
-		if [ $? -eq 0 ]; then 
-			echo "$service is running" >> $LOG_FILE
-		else
-			echo "ERROR: $service is not running" >> $LOG_FILE
-		fi
-	done
-
-# done : Marks the end of the loop 
-# < $DEVICES_FILE : Feeds the content of the input file into the loop
+# Marks the end of the loop and feeds the content of the input file into the loop
 done < $DEVICES_FILE
+
+# Run system memory check
+echo -e "\nRunning System Memory Check - $(date)" >> $LOG_FILE
+
+# Display the disk space usage in a human-readable format(-h)
+echo "Disk Usage:" >> $LOG_FILE
+df -h >> $LOG_FILE
+# Shows memory usage in megabytes(-m)
+echo "MEMORY USAGE:" >> $LOG_FILE
+free -m >> $LOG_FILE
+
+# Run service check 
+echo -e "\nRunning Service Check - $(date)" >> $LOG_FILE
+
+# Iterate through each service in the SERVICES array
+for service in "${SERVICES[@]}"; do
+	# Checks if the service is runnning
+	systemctl is-active --quiet $service
+	# Check if systemctl returns success or fail
+	if [ $? -eq 0 ]; then 
+		echo "$service is running" >> $LOG_FILE
+	else
+        	echo "ERROR: $service is not running" >> $LOG_FILE
+	fi
+done
+
 
 echo "Diagnostics complete. Results saved to $LOG_FILE"
